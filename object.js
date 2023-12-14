e953e6cebf9eb5ef359aa851ea96eab01449c0e8
Random <rc786986@gmail.com>
2023-12-14 22:47:03 +0530

    old projects to remember

---
+++ object.js
@@ -0,0 +1,12 @@
+let hr = [
+    {
+        name : "Hariom",age : 20,sname : "Dhakad"},
+    {
+        name : "Hariom",age : 20,sname : "Dhakad"},
+    {
+        name : "Hariom", age : 20,sname : "Dhakad"},
+]
+for(i=0; i<hr.length; i++)
+{
+    hr.filter()
+}
